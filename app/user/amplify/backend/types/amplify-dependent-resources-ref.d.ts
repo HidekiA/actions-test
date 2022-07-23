548c125a1b5fc6010b76cde422e93cbf3d3da068
Hideki Aoyama <aoyama.hideki@jp.panasonic.com>
2022-07-24 05:23:40 +0900

    add app

---
+++ app/user/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "userPreSignup": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "auth": {
+        "user": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
